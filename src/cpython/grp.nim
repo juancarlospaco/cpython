d002b82ee5ba986bdbc299eb2c1f81ed9a63d0de
Juan Carlos <juancarlospaco@gmail.com>
2021-07-16 17:34:40 -0300

    init

--- src/cpython/grp.nim
+++ src/cpython/grp.nim
@@ -6,21 +6,6 @@ template X(simbol): auto =
   when declared result: module.simbol().to(type(result)) else: discard module.simbol()
 template X(simbol; a): auto =
   when declared result: module.simbol(a).to(type(result)) else: discard module.simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: module.simbol(a, b).to(type(result)) else: discard module.simbol(a, b)
-template X(simbol; a, b, c): auto =
-  when declared result: module.simbol(a, b, c).to(type(result)) else: discard module.simbol(a, b, c)
-template X(simbol; a, b, c, d): auto =
-  when declared result: module.simbol(a, b, c, d).to(type(result)) else: discard module.simbol(a, b, c, d)
-template X(simbol; a, b, c, d, e): auto =
-  when declared result: module.simbol(a, b, c, d, e).to(type(result)) else: discard module.simbol(a, b, c, d, e)
-template X(simbol; a, b, c, d, e, f): auto =
-  when declared result: module.simbol(a, b, c, d, e, f).to(type(result)) else: discard module.simbol(a, b, c, d, e, f)
-template X(simbol; a, b, c, d, e, f, g): auto =
-  when declared result: module.simbol(a, b, c, d, e, f, g).to(type(result)) else: discard module.simbol(a, b, c, d, e, f, g)
-template X(simbol; a, b, c, d, e, f, g, h): auto =
-  when declared result: module.simbol(a, b, c, d, e, f, g, h).to(type(result)) else: discard module.simbol(a, b, c, d, e, f, g, h)
-
 proc getgrgid*(gid: int): tuple[gr_name, gr_passwd: string, gr_gid: int, gr_mem: seq[string]]     = X getgrgid, gid
 proc getgrnam*(name: string): tuple[gr_name, gr_passwd: string, gr_gid: int, gr_mem: seq[string]] = X getgrnam, name
 proc getgrall*(): seq[tuple[gr_name, gr_passwd: string, gr_gid: int, gr_mem: seq[string]]]        = X getgrall
