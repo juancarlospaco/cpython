b359ca8c086e7cf9b9e6c1214c8c7b0e6b0bc699
Juan Carlos <juancarlospaco@gmail.com>
2021-07-16 17:26:23 -0300

    init

---
+++ src/cpython/binascii.nim
@@ -0,0 +1,30 @@
+## * https://docs.python.org/3.10/library/binascii.html
+import nimpy
+const module = "binascii"
+template X(simbol; a): auto =
+  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
+template X(simbol; a, b): auto =
+  when declared result: pyImport(module).simbol(a, b).to(type(result)) else: discard pyImport(module).simbol(a, b)
+template X(simbol; a, b, c): auto =
+  when declared result: pyImport(module).simbol(a, b, c).to(type(result)) else: discard pyImport(module).simbol(a, b, c)
+template X(simbol; a, b, c, d): auto =
+  when declared result: pyImport(module).simbol(a, b, c, d).to(type(result)) else: discard pyImport(module).simbol(a, b, c, d)
+proc hexlify*(data: string):        string = X hexlify, data
+proc a2b_hex*(data: string):        string = X a2b_hex, data
+proc unhexlify*(data: string):      string = X unhexlify, data
+proc b2a_hex*(data: string):        string = X b2a_hex, data
+proc a2b_uu*(s: string):            string = X a2b_uu, s
+proc b2a_uu*(s: string):            string = X b2a_uu, s
+proc a2b_base64*(s: string):        string = X a2b_base64, s
+proc b2a_base64*(s: string):        string = X b2a_base64, s
+proc a2b_hqx*(s: string):           string = X a2b_hqx, s
+proc rledecode_hqx*(s: string):     string = X rledecode_hqx, s
+proc rlecode_hqx*(s: string):       string = X rlecode_hqx, s
+proc b2a_hqx*(s: string):           string = X b2a_hqx, s
+proc crc_hqx*(data, value: string): string = X crc_hqx, data, value
+proc crc32*(data, value: string):   string = X crc32, data, value
+proc crc32*(data: string):          string = X crc32, data
+proc a2b_qp*(s: string; header=false): string = X a2b_qp, s, header
+proc b2a_hex*(data: string; sep: char; bytes_per_sep = 1): string = X b2a_hex, data, sep, bytes_per_sep
+proc hexlify*(data: string; sep: char; bytes_per_sep = 1): string = X hexlify, data, sep, bytes_per_sep
+proc b2a_qp*(s: string; quotetabs=false; istext=true, header=false): string = X b2a_qp, s, quotetabs, istext, header
