cec506b00ddaa3b1af5e90dbd5c9a97e1235c022
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 02:18:27 -0300

    init

---
+++ src/cpython/bz2.nim
@@ -0,0 +1,10 @@
+## * https://docs.python.org/3.10/library/bz2.html
+from std/os import splitFile
+import nimpy
+let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
+template X(simbol; a): auto =
+  when declared result: module.simbol(a).to(type(result)) else: discard module.simbol(a)
+template X(simbol; a, b): auto =
+  when declared result: module.simbol(a, b).to(type(result)) else: discard module.simbol(a, b)
+proc compress*(data: string; compresslevel = 9) = X compress, data, compresslevel
+proc decompress*(data: string)                  = X decompress, data
