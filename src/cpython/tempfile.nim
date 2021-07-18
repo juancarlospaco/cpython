1977f1c4864ed611c9c918d48cbc72d7bb0c1539
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:30:14 -0300

    Simplify code

--- src/cpython/tempfile.nim
+++ src/cpython/tempfile.nim
@@ -1,8 +1,10 @@
 ## * https://docs.python.org/3.10/library/tempfile.html
 import nimpy
+
 template X(simbol):          auto = nimpy.pyImport("tempfile").simbol().to(type(result))
 template X(simbol; a, b):    auto = nimpy.pyImport("tempfile").simbol(a, b).to(type(result))
 template X(simbol; a, b, c): auto = nimpy.pyImport("tempfile").simbol(a, b, c).to(type(result))
+
 proc mkdtemp*():                            string = X mkdtemp
 proc mkdtemp*(suffix, prefix: string):      string = X mkdtemp, suffix, prefix
 proc mkdtemp*(suffix, prefix, dir: string): string = X mkdtemp, suffix, prefix, dir
