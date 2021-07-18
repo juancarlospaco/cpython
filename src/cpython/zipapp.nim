7433e1cf56cf2ce2259d5cfefdf38a067dddbb73
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 23:16:03 -0300

    Simplify code

--- src/cpython/zipapp.nim
+++ src/cpython/zipapp.nim
@@ -1,15 +1,10 @@
 ## * https://docs.python.org/3.10/library/zipapp.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
+template X(simbol; a, b)       = discard nimpy.pyImport("zipapp").simbol(a, b)
+template X(simbol; a, b, c)    = discard nimpy.pyImport("zipapp").simbol(a, b, c)
+template X(simbol; a, b, c, d) = discard nimpy.pyImport("zipapp").simbol(a, b, c, d)
 template X(simbol; a): auto =
-  when declared result: module.simbol(a).to(type(result)) else: discard module.simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: module.simbol(a, b).to(type(result)) else: discard module.simbol(a, b)
-template X(simbol; a, b, c): auto =
-  when declared result: module.simbol(a, b, c).to(type(result)) else: discard module.simbol(a, b, c)
-template X(simbol; a, b, c, d): auto =
-  when declared result: module.simbol(a, b, c, d).to(type(result)) else: discard module.simbol(a, b, c, d)
+  when declared result: nimpy.pyImport("zipapp").simbol(a).to(type(result)) else: discard nimpy.pyImport("zipapp").simbol(a)
 proc create_archive*(source, target, interpreter, main: string) = X create_archive, source, target, interpreter, main
 proc create_archive*(source, target, interpreter: string) = X create_archive, source, target, interpreter
 proc create_archive*(source, target: string) = X create_archive, source, target
