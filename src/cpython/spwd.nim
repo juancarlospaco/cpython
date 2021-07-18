a3ee6fa4b5790956a1142bb24a92e5ac76beafe2
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:49:28 -0300

    Simplify code

--- src/cpython/spwd.nim
+++ src/cpython/spwd.nim
@@ -1,9 +1,6 @@
 ## * https://docs.python.org/3.10/library/spwd.html
 import nimpy
-const module = "spwd"
-template X(simbol): auto =
-  when declared result: pyImport(module).simbol().to(type(result)) else: discard pyImport(module).simbol()
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
+template X(simbol):    auto = nimpy.pyImport("runpy").simbol().to(type(result))
+template X(simbol; a): auto = nimpy.pyImport("runpy").simbol(a).to(type(result))
 proc getspnam*(name: string): tuple[sp_namp, sp_pwdp: string; sp_lstchg, sp_min, sp_max, sp_warn, sp_inact, sp_expire, sp_flag: int]  = X getspnam, name
 proc getspall*():         seq[tuple[sp_namp, sp_pwdp: string; sp_lstchg, sp_min, sp_max, sp_warn, sp_inact, sp_expire, sp_flag: int]] = X getspall
