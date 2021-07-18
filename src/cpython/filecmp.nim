6d863850adc7f2e37225355c33aab7f877b86946
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:32:54 -0300

    Simplify code

--- src/cpython/filecmp.nim
+++ src/cpython/filecmp.nim
@@ -1,8 +1,10 @@
 ## * https://docs.python.org/3.10/library/filecmp.html
 import nimpy
+
 template X(simbol)                   = discard nimpy.pyImport("filecmp").simbol()
 template X(simbol; a, b, c):    auto = nimpy.pyImport("filecmp").simbol(a, b, c).to(type(result))
 template X(simbol; a, b, c, d): auto = nimpy.pyImport("filecmp").simbol(a, b, c, d).to(type(result))
+
 proc clear_cache*() = X clear_cache
 proc cmp*(f1, f2: string, shallow=true): bool = X cmp, f1, f2, shallow
 proc cmpfiles*(dir1, dir2: string; common: seq[string]; shallow=true): tuple[match, mismatch, errors: seq[string]] = X cmpfiles, dir1, dir2, common, shallow
