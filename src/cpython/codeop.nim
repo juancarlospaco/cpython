c5a7852b7c9fa2c9d0138f3b21510e56b1d1ef18
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:34:36 -0300

    Simplify code

--- src/cpython/codeop.nim
+++ src/cpython/codeop.nim
@@ -1,4 +1,6 @@
 ## * https://docs.python.org/3.10/library/codeop.html
 import nimpy
+
 template X(simbol) = discard nimpy.pyImport("codeop").simbol(source, filename, symbol)
+
 proc compile_command*(source: string, filename="<input>", symbol="single") = X compile_command
