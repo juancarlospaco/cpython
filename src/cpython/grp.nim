c67657bff45246ca406f185d12a631e4bf8a12cc
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:12:01 -0300

    Simplify code

--- src/cpython/grp.nim
+++ src/cpython/grp.nim
@@ -1,11 +1,7 @@
 ## * https://docs.python.org/3.10/library/grp.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol): auto =
-  when declared result: module.simbol().to(type(result)) else: discard module.simbol()
-template X(simbol; a): auto =
-  when declared result: module.simbol(a).to(type(result)) else: discard module.simbol(a)
+template X(simbol):    auto =  nimpy.pyImport("grp").simbol( ).to(type(result))
+template X(simbol; a): auto =  nimpy.pyImport("grp").simbol(a).to(type(result))
 proc getgrgid*(gid: int): tuple[gr_name, gr_passwd: string, gr_gid: int, gr_mem: seq[string]]     = X getgrgid, gid
 proc getgrnam*(name: string): tuple[gr_name, gr_passwd: string, gr_gid: int, gr_mem: seq[string]] = X getgrnam, name
 proc getgrall*(): seq[tuple[gr_name, gr_passwd: string, gr_gid: int, gr_mem: seq[string]]]        = X getgrall
