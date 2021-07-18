7b48774e04cd4c3e09e8100171502a469666e4ec
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:28:50 -0300

    Simplify code

--- src/cpython/zipapp.nim
+++ src/cpython/zipapp.nim
@@ -1,10 +1,12 @@
 ## * https://docs.python.org/3.10/library/zipapp.html
 import nimpy
+
 template X(simbol; a, b)       = discard nimpy.pyImport("zipapp").simbol(a, b)
 template X(simbol; a, b, c)    = discard nimpy.pyImport("zipapp").simbol(a, b, c)
 template X(simbol; a, b, c, d) = discard nimpy.pyImport("zipapp").simbol(a, b, c, d)
 template X(simbol; a): auto =
   when declared result: nimpy.pyImport("zipapp").simbol(a).to(type(result)) else: discard nimpy.pyImport("zipapp").simbol(a)
+
 proc create_archive*(source, target, interpreter, main: string) = X create_archive, source, target, interpreter, main
 proc create_archive*(source, target, interpreter: string) = X create_archive, source, target, interpreter
 proc create_archive*(source, target: string) = X create_archive, source, target
