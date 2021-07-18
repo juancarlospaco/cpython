c29094678aae61c3a80c52bb4593bcc68e7daaee
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:31:22 -0300

    Simplify code

--- src/cpython/nis.nim
+++ src/cpython/nis.nim
@@ -1,6 +1,8 @@
 ## * https://docs.python.org/3.10/library/nis.html
 import nimpy
+
 template X(simbol):       auto = nimpy.pyImport("nis").simbol().to(type(result))
 template X(simbol; a, b): auto = nimpy.pyImport("nis").simbol(a, b).to(type(result))
+
 proc match*(key, mapname: string): seq[string] = X match, key, mapname
 proc get_default_domain*():        string      = X get_default_domain
