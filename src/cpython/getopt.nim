0915061712eba0c0161c943f9e8ddb4f7548e821
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:12:29 -0300

    Simplify code

--- src/cpython/getopt.nim
+++ src/cpython/getopt.nim
@@ -1,11 +1,7 @@
 ## * https://docs.python.org/3.10/library/getopt.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol; a, b): auto =
-  when declared result: module.simbol(a, b).to(type(result)) else: discard module.simbol(a, b)
-template X(simbol; a, b, c): auto =
-  when declared result: module.simbol(a, b, c).to(type(result)) else: discard module.simbol(a, b, c)
+template X(simbol; a, b):    auto = nimpy.pyImport("getopt").simbol(a, b).to(type(result))
+template X(simbol; a, b, c): auto = nimpy.pyImport("getopt").simbol(a, b, c).to(type(result))
 proc getopt*(args: seq[string]; shortopts: string; longopts: seq[string]): tuple[option: seq[tuple[key, value: string]], value: seq[string]] = X getopt, args, shortopts, longopts
 proc getopt*(args: seq[string]; shortopts: string): tuple[option: seq[tuple[key, value: string]], value: seq[string]] = X getopt, args, shortopts
 proc gnu_getopt*(args: seq[string]; shortopts: string; longopts: seq[string]): tuple[option: seq[tuple[key, value: string]], value: seq[string]] = X gnu_getopt, args, shortopts, longopts
