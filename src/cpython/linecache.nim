3d0b0ab1fb9a51acec26c322840a2c5604f6bbdf
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:31:53 -0300

    Simplify code

--- src/cpython/linecache.nim
+++ src/cpython/linecache.nim
@@ -1,8 +1,10 @@
 ## * https://docs.python.org/3.10/library/linecache.html
 import nimpy
+
 template X(simbol)             = discard nimpy.pyImport("linecache").simbol()
 template X(simbol; a):    auto = nimpy.pyImport("linecache").simbol(a).to(type(result))
 template X(simbol; a, b): auto = nimpy.pyImport("linecache").simbol(a, b).to(type(result))
+
 proc getline*(filename: string; lineno: int): string = X getline, filename, lineno
 proc clearcache*()                         = X clearcache
 proc checkcache*(filename: string): string = X checkcache, filename
