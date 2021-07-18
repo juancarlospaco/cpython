ef1f3c81e3cff6e6af842916e03983209229b264
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:28:55 -0300

    Simplify code

--- src/cpython/winsound.nim
+++ src/cpython/winsound.nim
@@ -1,8 +1,10 @@
 ## * https://docs.python.org/3.10/library/winsound.html
 import nimpy
+
 template X(simbol)       = discard nimpy.pyImport("winsound").simbol()
 template X(simbol; a)    = discard nimpy.pyImport("winsound").simbol(a)
 template X(simbol; a, b) = discard nimpy.pyImport("winsound").simbol(a, b)
+
 proc beep*(frequency, duration: int)       = X Beep, frequency, duration
 proc playSound*(sound: string; flags: int) = X PlaySound, sound, flags
 proc messageBeep*(tipe: int)               = X MessageBeep, tipe
