f01280fb5fbcc86f8c05bad0d203b2fd86b33986
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:29:57 -0300

    Simplify code

--- src/cpython/textwrap.nim
+++ src/cpython/textwrap.nim
@@ -1,7 +1,9 @@
 ## * https://docs.python.org/3.10/library/textwrap.html
 import nimpy
+
 template X(simbol; a):    auto = nimpy.pyImport("termios").simbol(a).to(type(result))
 template X(simbol; a, b): auto = nimpy.pyImport("termios").simbol(a, b).to(type(result))
+
 proc wrap*(text: string; width=70): string = X wrap, text, width
 proc fill*(text: string; width=70): string = X fill, text, width
 proc dedent*(text: string):         string = X dedent, text
