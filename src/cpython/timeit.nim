c57d208438d441b2c150a49e74db56982a6a6ca8
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:29:46 -0300

    Simplify code

--- src/cpython/timeit.nim
+++ src/cpython/timeit.nim
@@ -1,5 +1,7 @@
 ## * https://docs.python.org/3.10/library/timeit.html
 import nimpy
+
 template X(simbol; a, b) = discard nimpy.pyImport("timeit").simbol(a, b)
+
 proc timeit*(stmts="pass", setup="pass") = X timeit, stmts, setup
 proc repeat*(stmts="pass", setup="pass") = X repeat, stmts, setup
