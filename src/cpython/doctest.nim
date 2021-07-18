463ddf7cd3d77aaa9a6d6a0b1a4a9eb99626f529
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:33:23 -0300

    Simplify code

--- src/cpython/doctest.nim
+++ src/cpython/doctest.nim
@@ -1,10 +1,12 @@
 ## * https://docs.python.org/3.10/library/doctest.html
 import nimpy
+
 template X(simbol):             auto = nimpy.pyImport("doctest").simbol().to(type(result))
 template X(simbol; a):          auto = nimpy.pyImport("doctest").simbol(a).to(type(result))
 template X(simbol; a, b):       auto = nimpy.pyImport("doctest").simbol(a, b).to(type(result))
 template X(simbol; a, b, c):    auto = nimpy.pyImport("doctest").simbol(a, b, c).to(type(result))
 template X(simbol; a, b, c, d): auto = nimpy.pyImport("doctest").simbol(a, b, c, d).to(type(result))
+
 proc testmod*(): tuple[failure_count, test_count: int] = X testmod
 proc testfile*(filename: string; module_relative: bool; name, package: string): tuple[failure_count, test_count: int] = X testfile, filename, module_relative, name, package
 proc testfile*(filename: string; module_relative: bool; name: string):          tuple[failure_count, test_count: int] = X testfile, filename, module_relative, name
