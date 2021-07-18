46d6cba898b9e93db13726a8143fdc9e43ee97b7
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 23:16:09 -0300

    Simplify code

--- src/cpython/winsound.nim
+++ src/cpython/winsound.nim
@@ -1,13 +1,8 @@
 ## * https://docs.python.org/3.10/library/winsound.html
-from std/os import splitFile
-import nimpy  # Nimpy wont take varargs nor openArray nor T for all arguments at the time of writing.
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol): auto =
-  when declared result: module.simbol().to(type(result)) else: discard module.simbol()
-template X(simbol; a): auto =
-  when declared result: module.simbol(a).to(type(result)) else: discard module.simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: module.simbol(a, b).to(type(result)) else: discard module.simbol(a, b)
+import nimpy
+template X(simbol)       = discard nimpy.pyImport("winsound").simbol()
+template X(simbol; a)    = discard nimpy.pyImport("winsound").simbol(a)
+template X(simbol; a, b) = discard nimpy.pyImport("winsound").simbol(a, b)
 proc beep*(frequency, duration: int)       = X Beep, frequency, duration
 proc playSound*(sound: string; flags: int) = X PlaySound, sound, flags
 proc messageBeep*(tipe: int)               = X MessageBeep, tipe
