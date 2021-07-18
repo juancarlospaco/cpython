1362e6a9c7e5ae4247a745164e6a8a5d5237f0c4
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:34:13 -0300

    Simplify code

--- src/cpython/bz2.nim
+++ src/cpython/bz2.nim
@@ -1,6 +1,8 @@
 ## * https://docs.python.org/3.10/library/bz2.html
 import nimpy
+
 template X(simbol; a):    auto = nimpy.pyImport("bz2").simbol(a).to(type(result))
 template X(simbol; a, b): auto = nimpy.pyImport("bz2").simbol(a, b).to(type(result))
+
 proc compress*(data: string; compresslevel = 9): string = X compress, data, compresslevel
 proc decompress*(data: string): string                  = X decompress, data
