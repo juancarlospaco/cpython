49165288904994e990cc1bb7de6d1c0084b550da
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:49:16 -0300

    Simplify code

--- src/cpython/tabnanny.nim
+++ src/cpython/tabnanny.nim
@@ -1,6 +1,3 @@
 ## * https://docs.python.org/3.10/library/tabnanny.html
 import nimpy
-const module = "tabnanny"
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
-proc check*(file_or_dir: string) = X check, file_or_dir
+proc check*(file_or_dir: string) = discard nimpy.pyImport("tabnanny").check(file_or_dir)
