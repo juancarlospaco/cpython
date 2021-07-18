1db6944601dc6f89b87ec782c8c4718c1730fc6b
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:13:05 -0300

    Simplify code

--- src/cpython/doctest.nim
+++ src/cpython/doctest.nim
@@ -1,17 +1,10 @@
 ## * https://docs.python.org/3.10/library/doctest.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol): auto =
-  when declared result: module.simbol().to(type(result)) else: discard module.simbol()
-template X(simbol; a): auto =
-  when declared result: module.simbol(a).to(type(result)) else: discard module.simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: module.simbol(a, b).to(type(result)) else: discard module.simbol(a, b)
-template X(simbol; a, b, c): auto =
-  when declared result: module.simbol(a, b, c).to(type(result)) else: discard module.simbol(a, b, c)
-template X(simbol; a, b, c, d): auto =
-  when declared result: module.simbol(a, b, c, d).to(type(result)) else: discard module.simbol(a, b, c, d)
+template X(simbol):             auto = nimpy.pyImport("doctest").simbol().to(type(result))
+template X(simbol; a):          auto = nimpy.pyImport("doctest").simbol(a).to(type(result))
+template X(simbol; a, b):       auto = nimpy.pyImport("doctest").simbol(a, b).to(type(result))
+template X(simbol; a, b, c):    auto = nimpy.pyImport("doctest").simbol(a, b, c).to(type(result))
+template X(simbol; a, b, c, d): auto = nimpy.pyImport("doctest").simbol(a, b, c, d).to(type(result))
 proc testmod*(): tuple[failure_count, test_count: int] = X testmod
 proc testfile*(filename: string; module_relative: bool; name, package: string): tuple[failure_count, test_count: int] = X testfile, filename, module_relative, name, package
 proc testfile*(filename: string; module_relative: bool; name: string):          tuple[failure_count, test_count: int] = X testfile, filename, module_relative, name
