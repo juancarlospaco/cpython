ac806b1a09ee27c969adc3b0c9a2f3c863e79541
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:13:35 -0300

    Simplify code

--- src/cpython/codeop.nim
+++ src/cpython/codeop.nim
@@ -1,7 +1,4 @@
 ## * https://docs.python.org/3.10/library/codeop.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol; a, b, c): auto =
-  when declared result: module.simbol(a, b, c).to(type(result)) else: discard module.simbol(a, b, c)
-proc compile_command*(source: string, filename="<input>", symbol="single") = X compile_command, source, filename, symbol
+template X(simbol) = discard nimpy.pyImport("codeop").simbol(source, filename, symbol)
+proc compile_command*(source: string, filename="<input>", symbol="single") = X compile_command
