03b83cc33a60cb55922916463b1e6c43717ee001
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:29:02 -0300

    Simplify code

--- src/cpython/webbrowser.nim
+++ src/cpython/webbrowser.nim
@@ -1,5 +1,7 @@
 ## * https://docs.python.org/3.10/library/webbrowser.html
 import nimpy
+
 template X(simbol) = discard nimpy.pyImport("webbrowser").simbol(url)
+
 proc open_new*(url: string)     = X open_new
 proc open_new_tab*(url: string) = X open_new_tab
