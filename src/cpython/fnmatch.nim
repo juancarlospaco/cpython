fd7c3bc13833787220e3c67118d0cc34beed6bae
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:12:38 -0300

    Simplify code

--- src/cpython/fnmatch.nim
+++ src/cpython/fnmatch.nim
@@ -1,10 +1,7 @@
 ## * https://docs.python.org/3.10/library/fnmatch.html
 import nimpy
-const module = "fnmatch"
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: pyImport(module).simbol(a, b).to(type(result)) else: discard pyImport(module).simbol(a, b)
+template X(simbol; a):    auto = nimpy.pyImport("fnmatch").simbol(a).to(type(result))
+template X(simbol; a, b): auto = nimpy.pyImport("fnmatch").simbol(a, b).to(type(result))
 proc fnmatch*(filename, pattern: string):          bool        = X fnmatch, filename, pattern
 proc fnmatchcase*(filename, pattern: string):      bool        = X fnmatchcase, filename, pattern
 proc filter*(names: seq[string]; pattern: string): seq[string] = X filter, names, pattern
