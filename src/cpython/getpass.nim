04e7d6178d5e2b3627d33a5b643613d7e40c0393
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:32:34 -0300

    Simplify code

--- src/cpython/getpass.nim
+++ src/cpython/getpass.nim
@@ -1,6 +1,8 @@
 ## * https://docs.python.org/3.10/library/getpass.html
 import nimpy
+
 template X(simbol):    auto = nimpy.pyImport("getpass").simbol().to(type(result))
 template X(simbol; a): auto = nimpy.pyImport("getpass").simbol(a).to(type(result))
+
 proc getpass*(prompt = "Password: "): string = X getpass, prompt
 proc getuser*(): string = X getuser
