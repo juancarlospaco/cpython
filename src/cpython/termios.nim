62e4d3ba0b07b93fdaf77f86866ccf2760e97e26
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:48:59 -0300

    Simplify code

--- src/cpython/termios.nim
+++ src/cpython/termios.nim
@@ -1,10 +1,8 @@
 ## * https://docs.python.org/3.10/library/termios.html
 import nimpy
-const module = "termios"
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
+template X(simbol; a) = discard nimpy.pyImport("termios").simbol(a)
 template X(simbol; a, b): auto =
-  when declared result: pyImport(module).simbol(a, b).to(type(result)) else: discard pyImport(module).simbol(a, b)
+  when declared result: nimpy.pyImport("termios").simbol(a, b).to(type(result)) else: discard nimpy.pyImport("termios").simbol(a, b)
 proc tcgetattr*(fd: int): tuple[iflag, oflag, cflag, lflag, ispeed, ospeed: int; cc: seq[string]] = X tcgetattr, fd
 proc tcsendbreak*(fd, duration: int) = X tcsendbreak, fd, duration
 proc tcdrain*(fd: int) = X tcdrain, fd
