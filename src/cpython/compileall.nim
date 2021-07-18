792a5f7b1fc880fdf8cd0b75c7b079e15b80ff57
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:34:49 -0300

    Simplify code

--- src/cpython/compileall.nim
+++ src/cpython/compileall.nim
@@ -1,9 +1,11 @@
 ## * https://docs.python.org/3.10/library/compileall.html
 import nimpy
+
 template X(simbol; a):                auto = nimpy.pyImport("compileall").simbol(a).to(type(result))
 template X(simbol; a, b, c):          auto = nimpy.pyImport("compileall").simbol(a, b, c).to(type(result))
 template X(simbol; a, b, c, d):       auto = nimpy.pyImport("compileall").simbol(a, b, c, d).to(type(result))
 template X(simbol; a, b, c, d, e, f): auto = nimpy.pyImport("compileall").simbol(a, b, c, d, e, f).to(type(result))
+
 proc compile_dir*(dir: string; maxlevels: int; ddir: string; force=false): bool = X compile_dir, dir, maxlevels, ddir, force
 proc compile_dir*(dir: string): bool                                            = X compile_dir, dir
 proc compile_file*(fullname: string; ddir: string; force=false): bool = X compile_file, fullname, ddir, force
