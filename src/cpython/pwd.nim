aba3e3d23d00ba971f550038dd1a1c31afa5a3fe
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:31:16 -0300

    Simplify code

--- src/cpython/pwd.nim
+++ src/cpython/pwd.nim
@@ -1,7 +1,9 @@
 ## * https://docs.python.org/3.10/library/pwd.html
 import nimpy
+
 template X(simbol):    auto = nimpy.pyImport("pwd").simbol().to(type(result))
 template X(simbol; a): auto = nimpy.pyImport("pwd").simbol(a).to(type(result))
+
 proc getpwuid*(uid: int):     tuple[pw_name, pw_passwd: string, pw_uid, pw_gid: int, pw_gecos, pw_dir, pw_shell: string]  = X getpwuid, uid
 proc getpwnam*(name: string): tuple[pw_name, pw_passwd: string, pw_uid, pw_gid: int, pw_gecos, pw_dir, pw_shell: string]  = X getpwnam, name
 proc getpwall*():         seq[tuple[pw_name, pw_passwd: string, pw_uid, pw_gid: int, pw_gecos, pw_dir, pw_shell: string]] = X getpwall
