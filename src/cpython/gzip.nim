beb661f89fad9572a5a910ffab10f879cadfa3b6
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:32:05 -0300

    Simplify code

--- src/cpython/gzip.nim
+++ src/cpython/gzip.nim
@@ -1,6 +1,8 @@
 ## * https://docs.python.org/3.10/library/gzip.html
 import nimpy
+
 template X(simbol; a):    auto = nimpy.pyImport("gzip").simbol(a).to(type(result))
 template X(simbol; a, b): auto = nimpy.pyImport("gzip").simbol(a, b).to(type(result))
+
 proc compress*(data: string; compresslevel = 9): string = X compress, data, compresslevel
 proc decompress*(data: string): string                  = X decompress, data
