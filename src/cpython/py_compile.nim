4c222448bda66da42108d4a31538c170a6cf587f
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:31:10 -0300

    Simplify code

--- src/cpython/py_compile.nim
+++ src/cpython/py_compile.nim
@@ -1,8 +1,10 @@
 ## * https://docs.python.org/3.10/library/py_compile.html
 import nimpy
+
 template X(simbol; a):             auto = nimpy.pyImport("py_compile").simbol(a).to(type(result))
 template X(simbol; a, b):          auto = nimpy.pyImport("py_compile").simbol(a, b).to(type(result))
 template X(simbol; a, b, c, d, e): auto = nimpy.pyImport("py_compile").simbol(a, b, c, d, e).to(type(result))
+
 proc compile*(file, cfile, dfile: string; doraise=false; optimize= -1): string = X compile, file, cfile, dfile, doraise, optimize
 proc compile*(file, cfile: string): string = X compile, file, cfile
 proc compile*(file: string): string = X compile, file
