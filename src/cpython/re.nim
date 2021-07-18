fa73dad4bbed051c8fa0c2e079b1424a8597727c
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 14:39:49 -0300

    wip

--- src/cpython/re.nim
+++ src/cpython/re.nim
@@ -1,4 +1,4 @@
-## * https://docs.python.org/3.10/library/curses.html
+## * https://docs.python.org/3.10/library/re.html
 import nimpy
 
 template X(simbol)                      = discard nimpy.pyImport("re").simbol()
@@ -8,8 +8,8 @@ template X(simbol; a, b, c, d):    auto = nimpy.pyImport("re").simbol(a, b, c, d
 template X(simbol; a, b, c, d, e): auto = nimpy.pyImport("re").simbol(a, b, c, d, e).to(type(result))
 
 proc split*(pattern, str: string; maxsplit = 0, flags = 0): seq[string] = X split, pattern, str, maxsplit, flags
-proc findall*(pattern, str: string; flags = 0): seq[string] = X findall, pattern, str, flags
-proc sub*(pattern, repl, str: string, count=0, flags=0): string = X sub, pattern, repl, str, count, flags
+proc findall*(pattern, str: string; flags = 0):             seq[string] = X findall, pattern, str, flags
+proc sub*(pattern, repl, str: string, count=0, flags=0):    string      = X sub, pattern, repl, str, count, flags
 proc subn*(pattern, replacement: string; count=0, flags=0): tuple[new_string: string, number_of_subs_made: int] = X subn, pattern, replacement, count, flags
 proc escape*(pattern: string): string = X escape, pattern
 proc purge*() = X purge
