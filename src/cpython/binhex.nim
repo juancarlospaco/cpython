0d5645a81a5ecae7b690c7e014466ca67bc07ae8
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:14:06 -0300

    Simplify code

--- src/cpython/binhex.nim
+++ src/cpython/binhex.nim
@@ -1,7 +1,5 @@
 ## * https://docs.python.org/3.10/library/binhex.html
 import nimpy
-const module = "binhex"
-template X(simbol; a, b): auto =
-  when declared result: pyImport(module).simbol(a, b).to(type(result)) else: discard pyImport(module).simbol(a, b)
-proc binhex*(input, output: string) = X binhex, input, output
-proc hexbin*(input, output: string) = X hexbin, input, output
+template X(simbol) = discard nimpy.pyImport("binhex").simbol(input, output)
+proc binhex*(input, output: string) = X binhex
+proc hexbin*(input, output: string) = X hexbin
