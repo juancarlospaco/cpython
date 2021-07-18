f57f23de75d14ab9cbd56afcfbe700477b517ecd
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:48:45 -0300

    Simplify code

--- src/cpython/timeit.nim
+++ src/cpython/timeit.nim
@@ -1,7 +1,5 @@
 ## * https://docs.python.org/3.10/library/timeit.html
 import nimpy
-const module = "timeit"
-template X(simbol; a, b): auto =
-  when declared result: pyImport(module).simbol(a, b).to(type(result)) else: discard pyImport(module).simbol(a, b)
+template X(simbol; a, b) = discard nimpy.pyImport("timeit").simbol(a, b)
 proc timeit*(stmts="pass", setup="pass") = X timeit, stmts, setup
 proc repeat*(stmts="pass", setup="pass") = X repeat, stmts, setup
