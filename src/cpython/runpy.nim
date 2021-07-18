85418fa3333baa7a20dc7821c50f9be32270556f
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:30:49 -0300

    Simplify code

--- src/cpython/runpy.nim
+++ src/cpython/runpy.nim
@@ -1,5 +1,7 @@
 ## * https://docs.python.org/3.10/library/runpy.html
 import nimpy
+
 template X(simbol) = discard nimpy.pyImport("runpy").simbol(module_name)
+
 proc run_module*(module_name: string) = X run_module
 proc run_path*(module_name: string)   = X run_path
