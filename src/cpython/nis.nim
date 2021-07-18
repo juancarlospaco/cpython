ee71c007a62051b9c835436ba67cd519aa9e5809
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:49:56 -0300

    Simplify code

--- src/cpython/nis.nim
+++ src/cpython/nis.nim
@@ -1,10 +1,6 @@
 ## * https://docs.python.org/3.10/library/nis.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol): auto =
-  when declared result: module.simbol().to(type(result)) else: discard module.simbol()
-template X(simbol; a, b): auto =
-  when declared result: module.simbol(a, b).to(type(result)) else: discard module.simbol(a, b)
+template X(simbol):       auto = nimpy.pyImport("nis").simbol().to(type(result))
+template X(simbol; a, b): auto = nimpy.pyImport("nis").simbol(a, b).to(type(result))
 proc match*(key, mapname: string): seq[string] = X match, key, mapname
 proc get_default_domain*():        string      = X get_default_domain
