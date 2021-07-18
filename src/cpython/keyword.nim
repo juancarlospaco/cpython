f364caa17efa44351d2fd5bd98781efbcfad40b9
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:11:50 -0300

    Simplify code

--- src/cpython/keyword.nim
+++ src/cpython/keyword.nim
@@ -1,7 +1,5 @@
 ## * https://docs.python.org/3.10/library/keyword.html
 import nimpy
-const module = "keyword"
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
-proc iskeyword*(simbol: string): bool = X iskeyword, simbol
-proc issoftkeyword*(simbol: string): bool = X issoftkeyword, simbol
+template X(simbol): auto = nimpy.pyImport("keyword").simbol(symbol).to(type(result))
+proc iskeyword*(symbol: string):     bool = X iskeyword
+proc issoftkeyword*(symbol: string): bool = X issoftkeyword
