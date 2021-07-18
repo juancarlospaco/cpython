bb11f623ad82a52a65d8405c5d8ffa3625989aa0
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:49:43 -0300

    Simplify code

--- src/cpython/math.nim
+++ src/cpython/math.nim
@@ -1,25 +1,8 @@
 ## * https://docs.python.org/3.10/library/math.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol): auto =
-  when declared result: module.simbol().to(type(result)) else: discard module.simbol()
-template X(simbol; a): auto =
-  when declared result: module.simbol(a).to(type(result)) else: discard module.simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: module.simbol(a, b).to(type(result)) else: discard module.simbol(a, b)
-template X(simbol; a, b, c): auto =
-  when declared result: module.simbol(a, b, c).to(type(result)) else: discard module.simbol(a, b, c)
-template X(simbol; a, b, c, d): auto =
-  when declared result: module.simbol(a, b, c, d).to(type(result)) else: discard module.simbol(a, b, c, d)
-template X(simbol; a, b, c, d, e): auto =
-  when declared result: module.simbol(a, b, c, d, e).to(type(result)) else: discard module.simbol(a, b, c, d, e)
-template X(simbol; a, b, c, d, e, f): auto =
-  when declared result: module.simbol(a, b, c, d, e, f).to(type(result)) else: discard module.simbol(a, b, c, d, e, f)
-template X(simbol; a, b, c, d, e, f, g): auto =
-  when declared result: module.simbol(a, b, c, d, e, f, g).to(type(result)) else: discard module.simbol(a, b, c, d, e, f, g)
-template X(simbol; a, b, c, d, e, f, g, h): auto =
-  when declared result: module.simbol(a, b, c, d, e, f, g, h).to(type(result)) else: discard module.simbol(a, b, c, d, e, f, g, h)
+template X(simbol; a):                      auto = nimpy.pyImport("math").simbol(a).to(type(result))
+template X(simbol; a, b):                   auto = nimpy.pyImport("math").simbol(a, b).to(type(result))
+template X(simbol; a, b, c):                auto = nimpy.pyImport("math").simbol(a, b, c).to(type(result))
 proc ceil*(x: int or float):                  float = X ceil, x
 proc comb*(n, k: int or float):               float = X comb, n, k
 proc fabs*(x: int or float):                  float = X fabs, x
