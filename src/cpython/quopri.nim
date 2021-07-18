827be5464dd7a1348edb45410347f4d23e9cdbae
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:50:22 -0300

    Simplify code

--- src/cpython/quopri.nim
+++ src/cpython/quopri.nim
@@ -1,9 +1,6 @@
 ## * https://docs.python.org/3.10/library/quopri.html
 import nimpy
-const module = "quopri"
-template X(simbol; a, b): auto =
-  when declared result: pyImport(module).simbol(a, b).to(type(result)) else: discard pyImport(module).simbol(a, b)
-template X(simbol; a, b, c): auto =
-  when declared result: pyImport(module).simbol(a, b, c).to(type(result)) else: discard pyImport(module).simbol(a, b, c)
+template X(simbol; a, b):    auto = nimpy.pyImport("py_compile").simbol(a, b).to(type(result))
+template X(simbol; a, b, c): auto = nimpy.pyImport("py_compile").simbol(a, b, c).to(type(result))
 proc decodestring*(s: string; header=false):                  string = X decodestring, s, header
 proc encodestring*(s: string; quotetabs=false; header=false): string = X encodestring, s, quotetabs, header
