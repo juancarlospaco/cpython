faf0df7a112525af25a2604330013341a8e7069a
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:11:43 -0300

    Simplify code

--- src/cpython/linecache.nim
+++ src/cpython/linecache.nim
@@ -1,12 +1,8 @@
 ## * https://docs.python.org/3.10/library/linecache.html
 import nimpy
-const module = "linecache"
-template X(simbol): auto =
-  when declared result: pyImport(module).simbol().to(type(result)) else: discard pyImport(module).simbol()
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: pyImport(module).simbol(a, b).to(type(result)) else: discard pyImport(module).simbol(a, b)
+template X(simbol):       auto = discard nimpy.pyImport("linecache").simbol()
+template X(simbol; a):    auto = nimpy.pyImport("linecache").simbol(a).to(type(result))
+template X(simbol; a, b): auto = nimpy.pyImport("linecache").simbol(a, b).to(type(result))
 proc getline*(filename: string; lineno: int): string = X getline, filename, lineno
 proc clearcache*()                         = X clearcache
 proc checkcache*(filename: string): string = X checkcache, filename
