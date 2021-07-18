64e23025d8cfaa794f28323ed06d81bb352f150c
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:33:41 -0300

    Simplify code

--- src/cpython/crypt.nim
+++ src/cpython/crypt.nim
@@ -1,8 +1,10 @@
 ## * https://docs.python.org/3.10/library/crypt.html
 import nimpy
+
 template X(simbol):       auto = nimpy.pyImport("crypt").simbol().to(type(result))
 template X(simbol; a):    auto = nimpy.pyImport("crypt").simbol(a).to(type(result))
 template X(simbol; a, b): auto = nimpy.pyImport("crypt").simbol(a, b).to(type(result))
+
 proc crypt*(word: string):       string = X crypt, word
 proc crypt*(word, salt: string): string = X crypt, word, salt
 proc mksalt*():                  string = X mksalt
