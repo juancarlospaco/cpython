a5d32a39515d3435204ab9bdccb30f7a67819c07
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:29:38 -0300

    Simplify code

--- src/cpython/tty.nim
+++ src/cpython/tty.nim
@@ -1,5 +1,7 @@
 ## * https://docs.python.org/3.10/library/tty.html
 import nimpy
+
 template X(simbol) = discard nimpy.pyImport("tty").simbol(fd)
+
 proc setraw*(fd: int)    = X setraw
 proc setcbreak*(fd: int) = X setcbreak
