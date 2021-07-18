22f26eb11b778abf2163ab23af5c4108e006ecf5
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:34:26 -0300

    Simplify code

--- src/cpython/codecs.nim
+++ src/cpython/codecs.nim
@@ -1,5 +1,7 @@
 ## * https://docs.python.org/3.10/library/codecs.html
 import nimpy
+
 template X(simbol): auto = nimpy.pyImport("codecs").simbol(obj, encoding, errors).to(type(result))
+
 proc encode*(obj: string; encoding = "utf-8", errors = "strict"): string = X encode
 proc decode*(obj: string; encoding = "utf-8", errors = "strict"): string = X decode
