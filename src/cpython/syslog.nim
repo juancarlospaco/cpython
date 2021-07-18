b7de01c84157fd4fc16225117dfd001f78933f5e
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:49:22 -0300

    Simplify code

--- src/cpython/syslog.nim
+++ src/cpython/syslog.nim
@@ -1,18 +1,13 @@
 ## * https://docs.python.org/3.10/library/syslog.html
 import nimpy
-const module = "syslog"
-template X(simbol): auto =
-  when declared result: pyImport(module).simbol().to(type(result)) else: discard pyImport(module).simbol()
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: pyImport(module).simbol(a, b).to(type(result)) else: discard pyImport(module).simbol(a, b)
-template X(simbol; a, b, c): auto =
-  when declared result: pyImport(module).simbol(a, b, c).to(type(result)) else: discard pyImport(module).simbol(a, b, c)
+template X(simbol)          = discard nimpy.pyImport("syslog").simbol()
+template X(simbol; a)       = discard nimpy.pyImport("syslog").simbol(a)
+template X(simbol; a, b)    = discard nimpy.pyImport("syslog").simbol(a, b)
+template X(simbol; a, b, c) = discard nimpy.pyImport("syslog").simbol(a, b, c)
+proc closelog*()                        = X closelog
+proc openlog*()                         = X openlog
 proc syslog*(message: string)           = X syslog, message
 proc syslog*(priority, message: string) = X syslog, priority, message
-proc openlog*()                                                 = X openlog
 proc openlog*(idents: string)                                   = X openlog, idents
 proc openlog*(idents: string; logoption: int)                   = X openlog, idents, logoption
 proc openlog*(idents: string; logoption: int; facility: string) = X openlog, idents, logoption, facility
-proc closelog*() = X closelog
