9c84abb0e850ed3658e7c47e23a9cd9e8f406763
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:50:09 -0300

    Simplify code

--- src/cpython/py_compile.nim
+++ src/cpython/py_compile.nim
@@ -1,12 +1,8 @@
 ## * https://docs.python.org/3.10/library/py_compile.html
 import nimpy
-const module = "py_compile"
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: pyImport(module).simbol(a, b).to(type(result)) else: discard pyImport(module).simbol(a, b)
-template X(simbol; a, b, c, d, e): auto =
-  when declared result: pyImport(module).simbol(a, b, c, d, e).to(type(result)) else: discard pyImport(module).simbol(a, b, c, d, e)
+template X(simbol; a):             auto = nimpy.pyImport("py_compile").simbol(a).to(type(result))
+template X(simbol; a, b):          auto = nimpy.pyImport("py_compile").simbol(a, b).to(type(result))
+template X(simbol; a, b, c, d, e): auto = nimpy.pyImport("py_compile").simbol(a, b, c, d, e).to(type(result))
 proc compile*(file, cfile, dfile: string; doraise=false; optimize= -1): string = X compile, file, cfile, dfile, doraise, optimize
 proc compile*(file, cfile: string): string = X compile, file, cfile
 proc compile*(file: string): string = X compile, file
