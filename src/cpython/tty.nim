ae4acc0aa67418e8de5e9181fc132b78e33f9fb2
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:48:39 -0300

    Simplify code

--- src/cpython/tty.nim
+++ src/cpython/tty.nim
@@ -1,7 +1,5 @@
 ## * https://docs.python.org/3.10/library/tty.html
 import nimpy
-const module = "tty"
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
-proc setraw*(fd: int)    = X setraw, fd
-proc setcbreak*(fd: int) = X setcbreak, fd
+template X(simbol) = discard nimpy.pyImport("tty").simbol(fd)
+proc setraw*(fd: int)    = X setraw
+proc setcbreak*(fd: int) = X setcbreak
