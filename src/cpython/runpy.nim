8ac2275c07352fc31bf12f70ba980faed2e62597
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:50:29 -0300

    Simplify code

--- src/cpython/runpy.nim
+++ src/cpython/runpy.nim
@@ -1,7 +1,5 @@
 ## * https://docs.python.org/3.10/library/runpy.html
 import nimpy
-const module = "runpy"
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
-proc run_module*(module_name: string) = X run_module, module_name
-proc run_path*(module_name: string)   = X run_path, module_name
+template X(simbol) = discard nimpy.pyImport("runpy").simbol(module_name)
+proc run_module*(module_name: string) = X run_module
+proc run_path*(module_name: string)   = X run_path
