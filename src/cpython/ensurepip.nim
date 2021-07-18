b56feaac284e68c5e1c8b201bc8c3cadc2198948
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:13:00 -0300

    Simplify code

--- src/cpython/ensurepip.nim
+++ src/cpython/ensurepip.nim
@@ -1,10 +1,8 @@
 ## * https://docs.python.org/3.10/library/ensurepip.html
 import nimpy
-const module = "ensurepip"
 template X(simbol): auto =
-  when declared result: pyImport(module).simbol().to(type(result)) else: discard pyImport(module).simbol()
-template X(simbol; a, b, c, d, e, f): auto =
-  when declared result: pyImport(module).simbol(a, b, c, d, e, f).to(type(result)) else: discard pyImport(module).simbol(a, b, c, d, e, f)
+  when declared result: nimpy.pyImport("ensurepip").simbol().to(type(result)) else: discard nimpy.pyImport("ensurepip").simbol()
+template X(simbol; a, b, c, d, e, f) = discard nimpy.pyImport("ensurepip").simbol(a, b, c, d, e, f)
 proc version*(): string = X version
 proc bootstrap*(root: string; upgrade=false; user=false; altinstall=false; default_pip=false; verbosity=0) = X bootstrap, root, upgrade, user, altinstall, default_pip, verbosity
 proc bootstrap*() = X bootstrap
