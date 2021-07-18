e13dccb6dce69bf51e779ac2d6370e36fa5bf7e3
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:13:41 -0300

    Simplify code

--- src/cpython/codecs.nim
+++ src/cpython/codecs.nim
@@ -1,8 +1,5 @@
 ## * https://docs.python.org/3.10/library/codecs.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol; a, b, c): auto =
-  when declared result: module.simbol(a, b, c).to(type(result)) else: discard module.simbol(a, b, c)
-proc encode*(obj: string; encoding="utf-8", errors="strict"): string = X encode, obj, encoding, errors
-proc decode*(obj: string; encoding="utf-8", errors="strict"): string = X decode, obj, encoding, errors
+template X(simbol): auto = nimpy.pyImport("codecs").simbol(obj, encoding, errors).to(type(result))
+proc encode*(obj: string; encoding = "utf-8", errors = "strict"): string = X encode
+proc decode*(obj: string; encoding = "utf-8", errors = "strict"): string = X decode
