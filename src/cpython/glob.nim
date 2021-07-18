fbfe18d9fee5d5cdb3d49aee4c2c9cf3974c99bb
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:32:18 -0300

    Simplify code

--- src/cpython/glob.nim
+++ src/cpython/glob.nim
@@ -1,5 +1,7 @@
 ## * https://docs.python.org/3.10/library/glob.html
 import nimpy
+
 template X(simbol; a): auto = nimpy.pyImport("glob").simbol(a).to(type(result))
+
 proc glob*(pathname: string):   seq[string] = X glob,   pathname
 proc escape*(pathname: string): string      = X escape, pathname
