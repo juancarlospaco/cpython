f576c2dce4d902d1f9d82f32b35a6196a40cef68
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:13:24 -0300

    Simplify code

--- src/cpython/compileall.nim
+++ src/cpython/compileall.nim
@@ -1,14 +1,9 @@
 ## * https://docs.python.org/3.10/library/compileall.html
 import nimpy
-const module = "compileall"
-template X(simbol; a): auto =
-  when declared result: pyImport(module).simbol(a).to(type(result)) else: discard pyImport(module).simbol(a)
-template X(simbol; a, b, c): auto =
-  when declared result: pyImport(module).simbol(a, b, c).to(type(result)) else: discard pyImport(module).simbol(a, b, c)
-template X(simbol; a, b, c, d): auto =
-  when declared result: pyImport(module).simbol(a, b, c, d).to(type(result)) else: discard pyImport(module).simbol(a, b, c, d)
-template X(simbol; a, b, c, d, e, f): auto =
-  when declared result: pyImport(module).simbol(a, b, c, d, e, f).to(type(result)) else: discard pyImport(module).simbol(a, b, c, d, e, f)
+template X(simbol; a):                auto = nimpy.pyImport("compileall").simbol(a).to(type(result))
+template X(simbol; a, b, c):          auto = nimpy.pyImport("compileall").simbol(a, b, c).to(type(result))
+template X(simbol; a, b, c, d):       auto = nimpy.pyImport("compileall").simbol(a, b, c, d).to(type(result))
+template X(simbol; a, b, c, d, e, f): auto = nimpy.pyImport("compileall").simbol(a, b, c, d, e, f).to(type(result))
 proc compile_dir*(dir: string; maxlevels: int; ddir: string; force=false): bool = X compile_dir, dir, maxlevels, ddir, force
 proc compile_dir*(dir: string): bool                                            = X compile_dir, dir
 proc compile_file*(fullname: string; ddir: string; force=false): bool = X compile_file, fullname, ddir, force
