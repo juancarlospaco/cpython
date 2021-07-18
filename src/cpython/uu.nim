eb399e9b5d733d7ee54f6f7555e566e263eb00a2
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:29:25 -0300

    Simplify code

--- src/cpython/uu.nim
+++ src/cpython/uu.nim
@@ -1,7 +1,9 @@
 ## * https://docs.python.org/3.10/library/uu.html
 import nimpy
+
 template X(simbol; a, b)       = discard nimpy.pyImport("uu").simbol(a, b)
 template X(simbol; a, b, c, d) = discard nimpy.pyImport("uu").simbol(a, b, c, d)
+
 proc encode*(in_file, out_file, name, mode: string)        = X encode, in_file, out_file, name, mode
 proc encode*(in_file, out_file: string)                    = X encode, in_file, out_file
 proc decode*(in_file, out_file, mode: string; quiet=false) = X decode, in_file, out_file, mode, quiet
