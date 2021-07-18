a41963553ee609c35527e64c836a2ec75917bff5
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 23:16:21 -0300

    Simplify code

--- src/cpython/warnings.nim
+++ src/cpython/warnings.nim
@@ -1,11 +1,8 @@
 ## * https://docs.python.org/3.10/library/warnings.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol): auto =
-  when declared result: module.simbol().to(type(result)) else: discard module.simbol()
+template X(simbol) = discard nimpy.pyImport("warnings").simbol()
 template X(simbol; a): auto =
-  when declared result: module.simbol(a).to(type(result)) else: discard module.simbol(a)
+  when declared result: nimpy.pyImport("warnings").simbol(a).to(type(result)) else: discard nimpy.pyImport("warnings").simbol(a)
 proc warn*(message: string) = X warn, message
 proc warn_explicit*(message: string) = X warn_explicit, message
 proc formatwarning*(message: string): string = X formatwarning, message
