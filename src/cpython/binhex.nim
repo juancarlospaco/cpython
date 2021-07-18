64561a04e1cd5577823ee070501c5eb4c23a5017
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:34:07 -0300

    Simplify code

--- src/cpython/binhex.nim
+++ src/cpython/binhex.nim
@@ -1,5 +1,7 @@
 ## * https://docs.python.org/3.10/library/binhex.html
 import nimpy
+
 template X(simbol) = discard nimpy.pyImport("binhex").simbol(input, output)
+
 proc binhex*(input, output: string) = X binhex
 proc hexbin*(input, output: string) = X hexbin
