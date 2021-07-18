3d29402eb9f5abc65c0a3b2ad1ba14f3195b0947
Juan Carlos <juancarlospaco@gmail.com>
2021-07-18 00:31:01 -0300

    Simplify code

--- src/cpython/quopri.nim
+++ src/cpython/quopri.nim
@@ -1,6 +1,8 @@
 ## * https://docs.python.org/3.10/library/quopri.html
 import nimpy
+
 template X(simbol; a, b):    auto = nimpy.pyImport("py_compile").simbol(a, b).to(type(result))
 template X(simbol; a, b, c): auto = nimpy.pyImport("py_compile").simbol(a, b, c).to(type(result))
+
 proc decodestring*(s: string; header=false):                  string = X decodestring, s, header
 proc encodestring*(s: string; quotetabs=false; header=false): string = X encodestring, s, quotetabs, header
