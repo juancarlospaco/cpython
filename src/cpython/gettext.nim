6c0f5467bb7515541dd806aba44563fe618f6034
Juan Carlos <juancarlospaco@gmail.com>
2021-07-17 22:12:17 -0300

    Simplify code

--- src/cpython/gettext.nim
+++ src/cpython/gettext.nim
@@ -1,15 +1,10 @@
 ## * https://docs.python.org/3.10/library/gettext.html
-from std/os import splitFile
 import nimpy
-let module = nimpy.pyImport(static(currentSourcePath().splitFile.name))
-template X(simbol): auto =
-  when declared result: module.simbol().to(type(result)) else: discard module.simbol()
+template X(simbol):          auto = discard nimpy.pyImport("gettext").simbol()
+template X(simbol; a, b)          = discard nimpy.pyImport("gettext").simbol(a, b)
+template X(simbol; a, b, c): auto = nimpy.pyImport("gettext").simbol(a, b, c).to(type(result))
 template X(simbol; a): auto =
-  when declared result: module.simbol(a).to(type(result)) else: discard module.simbol(a)
-template X(simbol; a, b): auto =
-  when declared result: module.simbol(a, b).to(type(result)) else: discard module.simbol(a, b)
-template X(simbol; a, b, c): auto =
-  when declared result: module.simbol(a, b, c).to(type(result)) else: discard module.simbol(a, b, c)
+  when declared result: nimpy.pyImport("gettext").simbol(a).to(type(result)) else: discard nimpy.pyImport("gettext").simbol(a)
 proc bindtextdomain*(domain, localedir: string)           = X bindtextdomain, domain, localedir
 proc bindtextdomain*(domain: string)                      = X bindtextdomain, domain
 proc bind_textdomain_codeset*(domain, codeset: string)    = X bind_textdomain_codeset, domain, codeset
