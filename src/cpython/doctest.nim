c007822ff69b45b59f52ab1085169b564ab2da9c
Juan Carlos <juancarlospaco@gmail.com>
2021-07-21 18:28:35 -0300

    Add constants

--- src/cpython/doctest.nim
+++ src/cpython/doctest.nim
@@ -6,6 +6,22 @@ template X(simbol; a):          auto = nimpy.pyImport("doctest").simbol(a).to(ty
 template X(simbol; a, b):       auto = nimpy.pyImport("doctest").simbol(a, b).to(type(result))
 template X(simbol; a, b, c):    auto = nimpy.pyImport("doctest").simbol(a, b, c).to(type(result))
 template X(simbol; a, b, c, d): auto = nimpy.pyImport("doctest").simbol(a, b, c, d).to(type(result))
+template `:=`(simbol; tipe: static[typedesc]) =
+  let simbol* {.inject.}: tipe = nimpy.pyImport("doctest").simbol.to(tipe)
+
+DONT_ACCEPT_TRUE_FOR_1    := int
+DONT_ACCEPT_BLANKLINE     := int
+NORMALIZE_WHITESPACE      := int
+ELLIPSIS                  := int
+IGNORE_EXCEPTION_DETAIL   := int
+SKIP                      := int
+COMPARISON_FLAGS          := int
+REPORT_UDIFF              := int
+REPORT_CDIFF              := int
+REPORT_NDIFF              := int
+REPORT_ONLY_FIRST_FAILURE := int
+FAIL_FAST                 := int
+REPORTING_FLAGS           := int
 
 proc testmod*(): tuple[failure_count, test_count: int] = X testmod
 proc testfile*(filename: string; module_relative: bool; name, package: string): tuple[failure_count, test_count: int] = X testfile, filename, module_relative, name, package
