e434fe3fcae12e43e7682037bc2a284d6db7bb0c
Juan Carlos <juancarlospaco@gmail.com>
2021-07-21 18:28:04 -0300

    Add constants

--- src/cpython/keyword.nim
+++ src/cpython/keyword.nim
@@ -2,6 +2,11 @@
 import nimpy
 
 template X(simbol): auto = nimpy.pyImport("keyword").simbol(symbol).to(type(result))
+template `:=`(simbol; tipe: typedesc) =
+  let simbol* {.inject.}: tipe = nimpy.pyImport("keyword").simbol.to(tipe)
+
+kwlist     := seq[string]
+softkwlist := seq[string]
 
 proc iskeyword*(symbol: string):     bool = X iskeyword
 proc issoftkeyword*(symbol: string): bool = X issoftkeyword
