cec9295caf918478f858aa8157d83802ca7296ab
Juan Carlos <juancarlospaco@gmail.com>
2021-07-21 19:03:10 -0300

    Add constants

--- src/cpython/tabnanny.nim
+++ src/cpython/tabnanny.nim
@@ -1,4 +1,10 @@
 ## * https://docs.python.org/3.10/library/tabnanny.html
 import nimpy
 
+template `:=`(simbol; tipe: static[typedesc]) =
+  let simbol* {.inject.}: tipe = nimpy.pyImport("tabnanny").simbol.to(tipe)
+
+verbose         := bool
+filename_only   := bool
+
 proc check*(file_or_dir: string) = discard nimpy.pyImport("tabnanny").check(file_or_dir)
