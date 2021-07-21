5083158c20f578a555c7853040335846a15ae0bb
Juan Carlos <juancarlospaco@gmail.com>
2021-07-21 18:28:24 -0300

    Add constants

--- src/cpython/filecmp.nim
+++ src/cpython/filecmp.nim
@@ -5,6 +5,8 @@ template X(simbol)                   = discard nimpy.pyImport("filecmp").simbol(
 template X(simbol; a, b, c):    auto = nimpy.pyImport("filecmp").simbol(a, b, c).to(type(result))
 template X(simbol; a, b, c, d): auto = nimpy.pyImport("filecmp").simbol(a, b, c, d).to(type(result))
 
+let DEFAULT_IGNORES*: seq[string] = nimpy.pyImport("filecmp").DEFAULT_IGNORES.to(seq[string])
+
 proc clear_cache*() = X clear_cache
 proc cmp*(f1, f2: string, shallow=true): bool = X cmp, f1, f2, shallow
 proc cmpfiles*(dir1, dir2: string; common: seq[string]; shallow=true): tuple[match, mismatch, errors: seq[string]] = X cmpfiles, dir1, dir2, common, shallow
