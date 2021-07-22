2b6b63fd959161b3a84a2d4b7bc905bf2f154502
Juan Carlos <juancarlospaco@gmail.com>
2021-07-22 16:17:26 -0300

    Add Doc

--- src/cpython/struct.nim
+++ src/cpython/struct.nim
@@ -9,5 +9,7 @@ template X(simbol; a, b, c, d): auto = discard nimpy.pyImport("struct").simbol(a
 proc calcsize*(format: string):                       int = X calcsize, format
 proc unpack*(format, buffer: string):              string = X unpack, format, buffer
 proc unpack_from*(format, buffer: string; offset=0)       = X unpack_from, format, buffer, offset
-proc pack*(format: string; values: varargs[auto]): string = X pack, format, values
-proc pack_into*(format: string; buffer: var string; offset: int; values: varargs[auto]) = X pack_into, format, buffer, offset, values
+
+when false:  # Nimpy does not like varargs ?.
+  proc pack*(format: string; values: varargs[auto]): string = X pack, format, values
+  proc pack_into*(format: string; buffer: var string; offset: int; values: varargs[auto]) = X pack_into, format, buffer, offset, values
