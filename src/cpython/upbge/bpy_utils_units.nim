c5d220809fe3b0fc3bd701988ca0ade7d7ce203f
Juan Carlos <juancarlospaco@gmail.com>
2021-08-10 14:32:52 -0300

    a

---
+++ src/cpython/upbge/bpy_utils_units.nim
@@ -0,0 +1,10 @@
+## * https://upbge.org/api/bpy.utils.units.html
+import nimpy
+
+template X(simbol; a, b, c):          auto = nimpy.pyImport("bpy.utils.units").simbol(a, b, c).to(type(result))
+template X(simbol; a, b, c, d):       auto = nimpy.pyImport("bpy.utils.units").simbol(a, b, c, d).to(type(result))
+template X(simbol; a, b, c, d, e, f): auto = nimpy.pyImport("bpy.utils.units").simbol(a, b, c, d, e, f).to(type(result))
+
+proc to_string*(unit_system, unit_category: string; value: float; precision=3; split_unit=false, compatible_unit=false): string = X to_string, unit_system, unit_category, value, precision, split_unit, compatible_unit
+proc to_value*(unit_system, unit_category, str_input, str_ref_unit: string): float = X to_value, unit_system, unit_category, str_input, str_ref_unit
+proc to_value*(unit_system, unit_category, str_input: string): float = X to_value, unit_system, unit_category, str_input
