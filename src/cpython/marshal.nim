e8dd30411cd6f87f77efbecb3ec5f52129d84562
Juan Carlos <juancarlospaco@gmail.com>
2021-12-28 16:49:37 -0800

    Finishing

--- src/cpython/marshal.nim
+++ src/cpython/marshal.nim
@@ -1,8 +1,6 @@
 ## * https://docs.python.org/3.10/library/marshal.html
 import nimpy
 
-template X(simbol; a):    auto = nimpy.pyImport("marshal").simbol(a).to(type(result))
 template X(simbol; a, b): auto = nimpy.pyImport("marshal").simbol(a, b).to(type(result))
 
 proc dumps*(value: auto; version: range[0..4] = 4): string = X dumps, value, version
-proc loads*(bites: string): auto = X loads, bites
