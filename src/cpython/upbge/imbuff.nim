06509f07d8457e2505e2230c2d74796f1ac36f61
Juan Carlos <juancarlospaco@gmail.com>
2022-08-19 20:04:48 -0300

    UPBGE 0.3+ support [skip ci]

--- src/cpython/upbge/imbuff.nim
+++ src/cpython/upbge/imbuff.nim
@@ -6,6 +6,6 @@ template X(simbol; a, b)    = discard nimpy.pyImport("imbuf").simbol(a, b)
 
 type ImBuf* = nimpy.PyObject  ## https://upbge.org/#/documentation/docs/latest/api/imbuf.types.html
 
-proc loadImBuf*(filepath: string): ImBuf = X load, filepath
-proc newImBuf*(size: array[2, int]): ImBuf = X new, size
+proc loadImBuf*(filepath: string):    ImBuf = X load, filepath
+proc newImBuf*(size: array[2, int]):  ImBuf = X new, size
 proc write*(image: ImBuf; filepath: string) = X write, image, filepath
