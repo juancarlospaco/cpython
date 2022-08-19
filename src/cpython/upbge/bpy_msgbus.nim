43e92db289aa9c673dc200a91b8eadaabb920514
Juan Carlos <juancarlospaco@gmail.com>
2022-08-19 20:04:35 -0300

    UPBGE 0.3+ support [skip ci]

--- src/cpython/upbge/bpy_msgbus.nim
+++ src/cpython/upbge/bpy_msgbus.nim
@@ -5,7 +5,7 @@ template X(simbol; a)             = discard nimpy.pyImport("bpy.msgbus").simbol(
 template X(simbol; a, b, c, d)    = discard nimpy.pyImport("bpy.msgbus").simbol(a, b, c, d)
 template X(simbol; a, b, c, d, e) = discard nimpy.pyImport("bpy.msgbus").simbol(a, b, c, d, e)
 
-proc clear_by_owner*(owner: auto) = X clear_by_owner, owner
-proc publish_rna*(key: auto) = X publish_rna, key
+proc clear_by_owner*(owner: auto)                            = X clear_by_owner, owner
+proc publish_rna*(key: auto)                                 = X publish_rna, key
 proc subscribe_rna*(key, owner, args, notify, options: auto) = X subscribe_rna, key, owner, args, notify, options
-proc subscribe_rna*(key, owner, args, notify: auto) = X subscribe_rna, key, owner, args, notify
+proc subscribe_rna*(key, owner, args, notify: auto)          = X subscribe_rna, key, owner, args, notify
