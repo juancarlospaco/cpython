ef140f4e78a15d1b9c1af05f376346fa85f9ddfc
Juan Carlos <juancarlospaco@gmail.com>
2022-09-18 17:29:19 -0300

    Python 3.12 support

--- src/cpython/tomllib.nim
+++ src/cpython/tomllib.nim
@@ -1,4 +1,5 @@
 ## https://docs.python.org/3.12/library/tomllib.html
+import std/tables
 import nimpy
 
 proc loads*(s: string): Table[string, string] = nimpy.pyImport("tomllib").loads(s).to(type(result))
