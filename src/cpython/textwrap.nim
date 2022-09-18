299997b7e4a5dc96b28f393f90e6522d7617b10b
Juan Carlos <juancarlospaco@gmail.com>
2022-09-18 18:09:20 -0300

    Python 3.12 support

--- src/cpython/textwrap.nim
+++ src/cpython/textwrap.nim
@@ -6,11 +6,10 @@ type TextWrapper* = nimpy.PyObject
 template X(simbol; a):    auto = nimpy.pyImport("textwrap").simbol(a).to(type(result))
 template X(simbol; a, b): auto = nimpy.pyImport("textwrap").simbol(a, b).to(type(result))
 
-proc wrap*(text: string; width = 70): string = X wrap, text, width
-proc fill*(text: string; width = 70): string = X fill, text, width
-proc dedent*(text: string):         string = X dedent, text
-proc indent*(text, prefix: string): string = X indent, text, prefix
-proc indent*(text: string):         string = X indent, text
+proc wrap*(text: string; width = 70):    string = X wrap, text, width
+proc fill*(text: string; width = 70):    string = X fill, text, width
+proc dedent*(text: string):              string = X dedent, text
+proc indent*(text, prefix: string):      string = X indent, text, prefix
+proc indent*(text: string):              string = X indent, text
 proc shorten*(text: string; width: int): string = X shorten, text, width
-
-proc newTextWrapper*(): TextWrapper = nimpy.pyImport("textwrap").TextWrapper()
+proc newTextWrapper*():             TextWrapper = nimpy.pyImport("textwrap").TextWrapper()
