6bd172f3b69eaa0a63c33af4892c4e329b0e0f90
Juan Carlos <juancarlospaco@gmail.com>
2022-09-18 15:14:02 -0300

    Python 3.12 support

--- src/cpython/timeit.nim
+++ src/cpython/timeit.nim
@@ -3,5 +3,5 @@ import nimpy
 
 template X(simbol; a, b) = discard nimpy.pyImport("timeit").simbol(a, b)
 
-proc timeit*(stmts="pass", setup="pass") = X timeit, stmts, setup
-proc repeat*(stmts="pass", setup="pass") = X repeat, stmts, setup
+proc timeit*(stmts = "pass", setup = "pass") = X timeit, stmts, setup
+proc repeat*(stmts = "pass", setup = "pass") = X repeat, stmts, setup
