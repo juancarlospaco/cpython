65d5a5048dd8a75e628963c8f07d0b4fab16fff3
Juan Carlos <juancarlospaco@gmail.com>
2022-09-18 15:12:33 -0300

    Python 3.12 support

--- src/cpython/textwrap.nim
+++ src/cpython/textwrap.nim
@@ -1,12 +1,18 @@
 ## * https://docs.python.org/3.10/library/textwrap.html
 import nimpy
 
-template X(simbol; a):    auto = nimpy.pyImport("termios").simbol(a).to(type(result))
-template X(simbol; a, b): auto = nimpy.pyImport("termios").simbol(a, b).to(type(result))
+type TextWrapper* = nimpy.PyObject
 
-proc wrap*(text: string; width=70): string = X wrap, text, width
-proc fill*(text: string; width=70): string = X fill, text, width
+template X(simbol; a):    auto = nimpy.pyImport("textwrap").simbol(a).to(type(result))
+template X(simbol; a, b): auto = nimpy.pyImport("textwrap").simbol(a, b).to(type(result))
+
+proc wrap*(text: string; width = 70): string = X wrap, text, width
+proc fill*(text: string; width = 70): string = X fill, text, width
 proc dedent*(text: string):         string = X dedent, text
 proc indent*(text, prefix: string): string = X indent, text, prefix
 proc indent*(text: string):         string = X indent, text
 proc shorten*(text: string; width: int): string = X shorten, text, width
+
+proc newTextWrapper*(): TextWrapper = nimpy.pyImport("textwrap").TextWrapper()
+proc wrap*(self: TextWrapper; text: string; width = 70): string = self.wrap(text, width)
+proc fill*(self: TextWrapper; text: string; width = 70): string = self.fill(text, width)
